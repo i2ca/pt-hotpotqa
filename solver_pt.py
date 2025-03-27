1c3d73ced4a2c391d458fb3a4988c0e2ce8b4de6
Sérgio Mucciaccia <smucciaccia@gmail.com>
2025-03-27 13:15:45 -0300

    Adding a portuguese solver

--- solver_pt.py
+++ solver_pt.py
@@ -1,18 +1,16 @@
 import os
 import json
 import time
-from datasets import load_dataset
+from datasets import Dataset
 from bedrock_api import BedrockApi
 from string_normalizer import StringNormalizer
-  
-hotpot_qa = load_dataset("hotpot_qa", "distractor")
-dataset = hotpot_qa['validation']
 
-print(dataset)
+dataset_path = "./datasets/x100_pt_hotpot_qa.json"
 
-seed = 853350
-shuffled = dataset.shuffle(seed=seed)
-subset = shuffled.select(range(100))
+with open(dataset_path, "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+subset = Dataset.from_list(data)
 
 temp_path = "./temp/temp_solution.json"
 id_set = set()
@@ -50,19 +48,19 @@ with open(temp_path, 'a', encoding='utf-8') as temp_file:
         answer = item["answer"]
         prompt = f"""\
 <prompt>
-Given the context below, answer the question concisely, providing only the direct answer without additional explanations or justifications.
+Dado o contexto abaixo, responda à pergunta de forma concisa, fornecendo apenas a resposta direta, sem explicações ou justificativas adicionais.
 </prompt>
 <context>
-The Eiffel Tower is located in the city of Paris, which is the capital of France. It was constructed in 1889.
+A Torre Eiffel está localizada na cidade de Paris, que é a capital da França. Ela foi construída em 1889.
 </context>
 <question>
-In which city is the Eiffel Tower located?
+Em que cidade está localizada a Torre Eiffel?
 </question>
 <answer>
 Paris
 </answer>
 <prompt>
-Given the context below, answer the question concisely, providing only the direct answer without additional explanations or justifications.
+Dado o contexto abaixo, responda à pergunta de forma concisa, fornecendo apenas a resposta direta, sem explicações ou justificativas adicionais.
 </prompt>
 <context>
 {full_context}
@@ -104,4 +102,4 @@ Given the context below, answer the question concisely, providing only the direc
         id_set.add(item["id"])
 
         print(f"Completion={normalized_completion}, Answer={normalized_answer}, Correct={n_correct}, Incorrect={n_incorrect}, {item['type']}-{item['level']}")
-        time.sleep(20)
+        time.sleep(30)
